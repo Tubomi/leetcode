7a03457fa37d6bd800ce863c0e1f38e524abcaa8
Tubomi <41150141+Tubomi@users.noreply.github.com>
2018-12-17 18:41:12 +0800

    Update two sum.py

--- two sum.py
+++ two sum.py
@@ -5,3 +5,34 @@
 #Given nums = [2, 7, 11, 15], target = 9,
 #Because nums[0] + nums[1] = 2 + 7 = 9,
 #return [0, 1].
+class Solution():
+    def twoSum(nums, target):
+        nums=list(nums)
+        target=int(target)
+        s=len(nums)
+        for i in range(s):
+            for j in range(i+1,s):
+                if nums[i]+nums[j]==target:
+                    #return[i,j] return 最终只返回一组，但是print可以返回所有可能情况
+                    print(i,j)
+                else:
+                    continue
+——————————————————————————————————————————————————————————————
+Solution.twoSum([1,2,3,7,4],5)
+
+#关于self
+class Solution():
+    def __init__(self,nums,target):
+        self.nums=list(nums)
+        self.target=int(target)
+    def Twosum(nums,target):
+        s=len(nums)
+        for i in range(s):
+            for j in range(i+1,s):
+                if nums[i]+nums[j]==target:
+                    print(i,j)
+                else:
+                    continue
+   _________________________________________________                 
+  Solution.Twosum([1,2,3,7,4,4],5)
+  #需改进，并没有判别是否为nums是否为list,target是否为int
